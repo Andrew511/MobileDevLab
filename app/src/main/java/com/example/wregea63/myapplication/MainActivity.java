773ed5bfa3cac53763d56916bb30b028d0a789cd
Andrew <Andrew511@users.noreply.github.com>
2018-03-05 21:18:15 -0600

    Work on lab3

--- app/src/main/java/com/example/wregea63/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wregea63/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 
@@ -28,14 +29,38 @@ public class MainActivity extends AppCompatActivity {
 
         TypedArray images = getResources().obtainTypedArray(R.array.cards);
         TypedArray handArray = getResources().obtainTypedArray(R.array.hand);
+        (findViewById(R.id.mathWarSubmit)).setEnabled(false);
 
         for (int i = 0; i < getResources().getInteger(R.integer.handSize); i++) {
             int choice = (int) (Math.random() * images.length());
-            ((ImageView)findViewById(handArray.getResourceId(i, R.id.card1))).setImageResource(images.getResourceId(choice, R.drawable.c1));
+            int cardChoice = images.getResourceId(choice, R.drawable.c1);
+            ImageView cardView = ((ImageView)findViewById(handArray.getResourceId(i, R.id.card1)));
+            cardView.setImageResource(cardChoice);
+            cardView.setTag(cardChoice);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Add your code in here!
+                    if (v.isSelected()) {
+                        if (findViewById(R.id.fieldCard1).getTag() != null) {
+                            ((ImageView)findViewById(R.id.fieldCard1)).setImageResource((int)v.getTag());
+                            (findViewById(R.id.fieldCard1)).setTag(v.getTag());
+                            v.setVisibility(View.GONE);
+                        }
+                        else if (findViewById(R.id.fieldCard2).getTag() != null) {
+                            ((ImageView)findViewById(R.id.fieldCard2)).setImageResource((int)v.getTag());
+                            (findViewById(R.id.fieldCard2)).setTag(v.getTag());
+                            v.setVisibility(View.GONE);
+                        }
+                    }
+                }
+            });
         }
         //images.recycle();
         //this leaves the keyboard hidden on load
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+
     }
 }
